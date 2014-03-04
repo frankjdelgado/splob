31f4e7227305733d9a7043f90c7754749ff4e313
Frank <frankjdelgado@gmail.com>
2014-03-04 17:07:24 -0430

    Back to top

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function smooth(){
+    var offset = 220;
+    var duration = 500;
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > offset) {
+            $('.back-to-top').fadeIn(duration);
+        } else {
+            $('.back-to-top').fadeOut(duration);
+        }
+    });
+    
+    $('.back-to-top').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
+}
+
+$(document).ready(function(){
+    smooth();
+    
+});
