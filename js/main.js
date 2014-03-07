53896e62f95fd7aeef90f8b515d1642ca1d4ff82
Frank <frankjdelgado@gmail.com>
2014-03-07 00:40:44 -0430

    Cambiar calendar a español

--- js/main.js
+++ js/main.js
@@ -17,10 +17,31 @@ function smooth(){
 }
 
 $(document).ready(function(){
-    smooth();
 
-    $('#calendar').fullCalendar({
-        // put your options and callbacks here
-    });
-    
+    smooth();
+    var options = {
+       allDayDefault:false,
+       header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay'
+       },
+       timeFormat: {
+        agenda: 'h(:mm)t{ - h(:mm)t}',
+        '': 'h(:mm)t{-h(:mm)t }'
+       },
+       monthNames: ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" ],
+       monthNamesShort: ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic'],
+       dayNames: [ 'Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+       dayNamesShort: ['Dom','Lun','Mar','Mié','Jue','Vie','Sáb'],
+       buttonText: {
+        today: 'hoy',
+        month: 'mes',
+        week: 'semana',
+        day: 'día'
+       }
+    };
+
+    $('#calendar').fullCalendar(options);
+
 });
