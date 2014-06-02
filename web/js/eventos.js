d0d7a66f5c5b7e66caa0ef1140fb747755b4788d
Frank <frankjdelgado@gmail.com>
2014-06-02 15:29:01 -0430

    agregar edicion de eventos

---
+++ web/js/eventos.js
@@ -0,0 +1,15 @@
+$(document).on('click','.evento-desc button',function(){
+
+	var des = $(this).parent().parent();
+	var parent = $(this).parent().parent().parent().find('.evento-edit');
+	$(parent).toggleClass('hide');
+	$(des).toggleClass('hide');
+});
+
+$(document).on('click','.evento-edit input[name="guardar"]',function(){
+	var parent_form = $(this).parent().parent();
+	var parent_desc = $(this).parent().parent().parent().find('.evento-desc');
+	$(parent_form).toggleClass('hide');
+	$(parent_desc).toggleClass('hide');
+});
+
