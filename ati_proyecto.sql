7d292bb11f9487c4815f241dfa134351c1895d51
Marvin Gonzalez <marvinjgh@gmail.com>
2014-06-06 08:08:45 -0430

    cambio del archivo sql

--- ati_proyecto.sql
+++ ati_proyecto.sql
@@ -1,86 +1,80 @@
-/*
-Navicat MySQL Data Transfer
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         5.5.35-MariaDB - mariadb.org binary distribution
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             8.0.0.4396
+-- --------------------------------------------------------
 
-Source Server         : PinzonDB
-Source Server Version : 50535
-Source Host           : localhost:3306
-Source Database       : ati_proyecto
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-Target Server Type    : MYSQL
-Target Server Version : 50535
-File Encoding         : 65001
+-- Volcando estructura de base de datos para ati_proyecto
+CREATE DATABASE IF NOT EXISTS `ati_proyecto` /*!40100 DEFAULT CHARACTER SET utf32 */;
+USE `ati_proyecto`;
 
-Date: 2014-06-05 12:05:17
-*/
 
-SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for t_categoria
--- ----------------------------
-DROP TABLE IF EXISTS `t_categoria`;
-CREATE TABLE `t_categoria` (
+-- Volcando estructura para tabla ati_proyecto.t_categoria
+CREATE TABLE IF NOT EXISTS `t_categoria` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id del hasgtag',
   `hashtag` varchar(255) DEFAULT '' COMMENT 'hashtag',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32;
 
--- ----------------------------
--- Records of t_categoria
--- ----------------------------
+-- Volcando datos para la tabla ati_proyecto.t_categoria: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `t_categoria` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_categoria` ENABLE KEYS */;
 
--- ----------------------------
--- Table structure for t_equipo
--- ----------------------------
-DROP TABLE IF EXISTS `t_equipo`;
-CREATE TABLE `t_equipo` (
+
+-- Volcando estructura para tabla ati_proyecto.t_equipo
+CREATE TABLE IF NOT EXISTS `t_equipo` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id del equipo',
   `nombre` varchar(255) DEFAULT '' COMMENT 'nombre del equipo',
   `pais` varchar(200) DEFAULT '' COMMENT 'pais del equipo',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf32;
 
--- ----------------------------
--- Records of t_equipo
--- ----------------------------
-INSERT INTO `t_equipo` VALUES ('1', '', 'Brasil');
-INSERT INTO `t_equipo` VALUES ('2', '', 'Argentina');
-INSERT INTO `t_equipo` VALUES ('3', '', 'Colombia');
-INSERT INTO `t_equipo` VALUES ('4', '', 'Uruguay');
-INSERT INTO `t_equipo` VALUES ('5', '', 'Belgica');
-INSERT INTO `t_equipo` VALUES ('6', '', 'Alemania');
-INSERT INTO `t_equipo` VALUES ('7', '', 'España');
-INSERT INTO `t_equipo` VALUES ('8', '', 'Suiza');
-INSERT INTO `t_equipo` VALUES ('9', '', 'Algeria');
-INSERT INTO `t_equipo` VALUES ('10', '', 'Camerun');
-INSERT INTO `t_equipo` VALUES ('11', '', 'Costa de Marfil');
-INSERT INTO `t_equipo` VALUES ('12', '', 'Ghana');
-INSERT INTO `t_equipo` VALUES ('13', '', 'Nigeria');
-INSERT INTO `t_equipo` VALUES ('14', '', 'Chile');
-INSERT INTO `t_equipo` VALUES ('15', '', 'Ecuador');
-INSERT INTO `t_equipo` VALUES ('16', '', 'Australia');
-INSERT INTO `t_equipo` VALUES ('17', '', 'Japon');
-INSERT INTO `t_equipo` VALUES ('18', '', 'Iran');
-INSERT INTO `t_equipo` VALUES ('19', '', 'Korea del Sur');
-INSERT INTO `t_equipo` VALUES ('20', '', 'Costa Rica');
-INSERT INTO `t_equipo` VALUES ('21', '', 'Honduras');
-INSERT INTO `t_equipo` VALUES ('22', '', 'México');
-INSERT INTO `t_equipo` VALUES ('23', '', 'Estados Unidos');
-INSERT INTO `t_equipo` VALUES ('24', '', 'Boznia y Herzegovina');
-INSERT INTO `t_equipo` VALUES ('25', '', 'Croacia');
-INSERT INTO `t_equipo` VALUES ('26', '', 'Inglaterra');
-INSERT INTO `t_equipo` VALUES ('27', '', 'Francia');
-INSERT INTO `t_equipo` VALUES ('28', '', 'Grecia');
-INSERT INTO `t_equipo` VALUES ('29', '', 'Italia');
-INSERT INTO `t_equipo` VALUES ('30', '', 'Holanda');
-INSERT INTO `t_equipo` VALUES ('31', '', 'Portugal');
-INSERT INTO `t_equipo` VALUES ('32', '', 'Rusia');
+-- Volcando datos para la tabla ati_proyecto.t_equipo: ~32 rows (aproximadamente)
+/*!40000 ALTER TABLE `t_equipo` DISABLE KEYS */;
+INSERT INTO `t_equipo` (`id`, `nombre`, `pais`) VALUES
+	(1, '', 'Brasil'),
+	(2, '', 'Argentina'),
+	(3, '', 'Colombia'),
+	(4, '', 'Uruguay'),
+	(5, '', 'Belgica'),
+	(6, '', 'Alemania'),
+	(7, '', 'España'),
+	(8, '', 'Suiza'),
+	(9, '', 'Algeria'),
+	(10, '', 'Camerun'),
+	(11, '', 'Costa de Marfil'),
+	(12, '', 'Ghana'),
+	(13, '', 'Nigeria'),
+	(14, '', 'Chile'),
+	(15, '', 'Ecuador'),
+	(16, '', 'Australia'),
+	(17, '', 'Japon'),
+	(18, '', 'Iran'),
+	(19, '', 'Korea del Sur'),
+	(20, '', 'Costa Rica'),
+	(21, '', 'Honduras'),
+	(22, '', 'México'),
+	(23, '', 'Estados Unidos'),
+	(24, '', 'Boznia y Herzegovina'),
+	(25, '', 'Croacia'),
+	(26, '', 'Inglaterra'),
+	(27, '', 'Francia'),
+	(28, '', 'Grecia'),
+	(29, '', 'Italia'),
+	(30, '', 'Holanda'),
+	(31, '', 'Portugal'),
+	(32, '', 'Rusia');
+/*!40000 ALTER TABLE `t_equipo` ENABLE KEYS */;
 
--- ----------------------------
--- Table structure for t_post
--- ----------------------------
-DROP TABLE IF EXISTS `t_post`;
-CREATE TABLE `t_post` (
+
+-- Volcando estructura para tabla ati_proyecto.t_post
+CREATE TABLE IF NOT EXISTS `t_post` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id del post',
   `imagen` varchar(1024) DEFAULT NULL COMMENT 'ubicacion del archivo',
   `contenido` varchar(255) DEFAULT NULL COMMENT 'contenido del psot',
@@ -93,33 +87,32 @@ CREATE TABLE `t_post` (
   CONSTRAINT `usuario_post_id` FOREIGN KEY (`usuario`) REFERENCES `t_usuario` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf32;
 
--- ----------------------------
--- Records of t_post
--- ----------------------------
-INSERT INTO `t_post` VALUES ('1', null, 'asdfasdf', '\0', '2014-06-03 20:30:18', null, '5');
-INSERT INTO `t_post` VALUES ('2', 'pentakill.jpg', 'contenido 2, ahora con imagen', '\0', '2014-06-03 20:35:57', null, '5');
-INSERT INTO `t_post` VALUES ('3', 'pentakill.jpg', 'asdf', '\0', '2014-06-03 21:59:45', null, '5');
-INSERT INTO `t_post` VALUES ('4', 'bg_champion_zed_1920x1080.jpg', 'asdfa asdf as', '\0', '2014-06-03 21:59:52', null, '5');
-INSERT INTO `t_post` VALUES ('5', 'shadow_isles_1920x1080.jpg', 'asdf', '', '2014-06-03 23:55:19', '2014-06-03 00:00:00', '5');
-INSERT INTO `t_post` VALUES ('6', null, 'asdf2', '', '2014-06-04 00:00:47', '2014-06-02 00:00:00', '5');
-INSERT INTO `t_post` VALUES ('7', 'bg_champion_mordekaiser_1920x1080.jpg', 'otro', '', '2014-06-04 00:21:14', '2014-06-04 00:00:00', '5');
-INSERT INTO `t_post` VALUES ('8', null, 'asdfasdfasdf', '', '2014-06-04 00:29:06', '2014-06-04 00:00:00', '5');
-INSERT INTO `t_post` VALUES ('10', null, 'prueba final si no estamos jodidos', '', '2014-06-04 00:37:42', '2014-06-04 00:00:00', '5');
-INSERT INTO `t_post` VALUES ('11', null, 'tendremos json?', '', '2014-06-04 00:56:07', '2014-06-04 00:00:00', '5');
-INSERT INTO `t_post` VALUES ('12', null, 'asdf', '', '2014-06-04 01:11:49', '2014-06-04 00:00:00', '5');
-INSERT INTO `t_post` VALUES ('13', null, 'ble ble', '\0', '2014-06-04 12:49:39', null, '5');
-INSERT INTO `t_post` VALUES ('14', null, 'ble ble', '\0', '2014-06-04 12:49:39', null, '5');
-INSERT INTO `t_post` VALUES ('15', null, 'bla bla bla bla ', '\0', '2014-06-04 12:52:25', null, '5');
-INSERT INTO `t_post` VALUES ('16', null, 'con ususario', '\0', '2014-06-04 19:56:21', null, '5');
-INSERT INTO `t_post` VALUES ('17', null, 'otra vez con usuario', '\0', '2014-06-04 20:01:03', null, '5');
-INSERT INTO `t_post` VALUES ('18', null, 'sera la buena?', '\0', '2014-06-04 20:05:03', null, '5');
-INSERT INTO `t_post` VALUES ('19', 'bg_champion_mordekaiser_1920x1080.jpg', ' sadfasdfsafd', '\0', '2014-06-04 20:11:45', null, '5');
+-- Volcando datos para la tabla ati_proyecto.t_post: ~18 rows (aproximadamente)
+/*!40000 ALTER TABLE `t_post` DISABLE KEYS */;
+INSERT INTO `t_post` (`id`, `imagen`, `contenido`, `tipo`, `fecha_creacion`, `fecha_calendario`, `usuario`) VALUES
+	(1, NULL, 'asdfasdf', b'00000000', '2014-06-03 20:30:18', NULL, 5),
+	(2, 'pentakill.jpg', 'contenido 2, ahora con imagen', b'00000000', '2014-06-03 20:35:57', NULL, 5),
+	(3, 'pentakill.jpg', 'asdf', b'00000000', '2014-06-03 21:59:45', NULL, 5),
+	(4, 'bg_champion_zed_1920x1080.jpg', 'asdfa asdf as', b'00000000', '2014-06-03 21:59:52', NULL, 5),
+	(5, 'shadow_isles_1920x1080.jpg', 'asdf', b'10000000', '2014-06-03 23:55:19', '2014-06-03 00:00:00', 5),
+	(6, NULL, 'asdf2', b'10000000', '2014-06-04 00:00:47', '2014-06-02 00:00:00', 5),
+	(7, 'bg_champion_mordekaiser_1920x1080.jpg', 'otro', b'10000000', '2014-06-04 00:21:14', '2014-06-04 00:00:00', 5),
+	(8, NULL, 'asdfasdfasdf', b'10000000', '2014-06-04 00:29:06', '2014-06-04 00:00:00', 5),
+	(10, NULL, 'prueba final si no estamos jodidos', b'10000000', '2014-06-04 00:37:42', '2014-06-04 00:00:00', 5),
+	(11, NULL, 'tendremos json?', b'10000000', '2014-06-04 00:56:07', '2014-06-04 00:00:00', 5),
+	(12, NULL, 'asdf', b'10000000', '2014-06-04 01:11:49', '2014-06-04 00:00:00', 5),
+	(13, NULL, 'ble ble', b'00000000', '2014-06-04 12:49:39', NULL, 5),
+	(14, NULL, 'ble ble', b'00000000', '2014-06-04 12:49:39', NULL, 5),
+	(15, NULL, 'bla bla bla bla ', b'00000000', '2014-06-04 12:52:25', NULL, 5),
+	(16, NULL, 'con ususario', b'00000000', '2014-06-04 19:56:21', NULL, 5),
+	(17, NULL, 'otra vez con usuario', b'00000000', '2014-06-04 20:01:03', NULL, 5),
+	(18, NULL, 'sera la buena?', b'00000000', '2014-06-04 20:05:03', NULL, 5),
+	(19, 'bg_champion_mordekaiser_1920x1080.jpg', ' sadfasdfsafd', b'00000000', '2014-06-04 20:11:45', NULL, 5);
+/*!40000 ALTER TABLE `t_post` ENABLE KEYS */;
 
--- ----------------------------
--- Table structure for t_post_categoria
--- ----------------------------
-DROP TABLE IF EXISTS `t_post_categoria`;
-CREATE TABLE `t_post_categoria` (
+
+-- Volcando estructura para tabla ati_proyecto.t_post_categoria
+CREATE TABLE IF NOT EXISTS `t_post_categoria` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'un id',
   `post` int(10) unsigned DEFAULT NULL COMMENT 'id del  post',
   `categoria` int(10) unsigned DEFAULT NULL COMMENT 'id de la categoria',
@@ -130,15 +123,13 @@ CREATE TABLE `t_post_categoria` (
   CONSTRAINT `post_id` FOREIGN KEY (`post`) REFERENCES `t_post` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COMMENT='tabla intermedia entre post y  categoria';
 
--- ----------------------------
--- Records of t_post_categoria
--- ----------------------------
+-- Volcando datos para la tabla ati_proyecto.t_post_categoria: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `t_post_categoria` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_post_categoria` ENABLE KEYS */;
 
--- ----------------------------
--- Table structure for t_usuario
--- ----------------------------
-DROP TABLE IF EXISTS `t_usuario`;
-CREATE TABLE `t_usuario` (
+
+-- Volcando estructura para tabla ati_proyecto.t_usuario
+CREATE TABLE IF NOT EXISTS `t_usuario` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `alias` varchar(64) DEFAULT '' COMMENT 'alias del usuario',
   `password` varchar(64) DEFAULT '' COMMENT 'contraseña del usuario',
@@ -146,16 +137,15 @@ CREATE TABLE `t_usuario` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf32;
 
--- ----------------------------
--- Records of t_usuario
--- ----------------------------
-INSERT INTO `t_usuario` VALUES ('5', 'marvin2', '8d969eef6ecad3c29a3a629280e686cf03f5d5a86aff3ca1200923adc6c92', 'marvin@asdf.com');
+-- Volcando datos para la tabla ati_proyecto.t_usuario: ~1 rows (aproximadamente)
+/*!40000 ALTER TABLE `t_usuario` DISABLE KEYS */;
+INSERT INTO `t_usuario` (`id`, `alias`, `password`, `email`) VALUES
+	(5, 'marvin2', '8d969eef6ecad3c29a3a629280e686cf03f5d5a86aff3ca1200923adc6c92', 'marvin@asdf.com');
+/*!40000 ALTER TABLE `t_usuario` ENABLE KEYS */;
 
--- ----------------------------
--- Table structure for t_usuario_equipo
--- ----------------------------
-DROP TABLE IF EXISTS `t_usuario_equipo`;
-CREATE TABLE `t_usuario_equipo` (
+
+-- Volcando estructura para tabla ati_proyecto.t_usuario_equipo
+CREATE TABLE IF NOT EXISTS `t_usuario_equipo` (
   `id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'un id',
   `usuario` int(10) unsigned DEFAULT NULL COMMENT 'usuario que tiene un equipo favorito',
   `equipo` int(10) unsigned DEFAULT NULL COMMENT 'equipo favorito de un ususario',
@@ -166,15 +156,18 @@ CREATE TABLE `t_usuario_equipo` (
   CONSTRAINT `usuario_id` FOREIGN KEY (`usuario`) REFERENCES `t_usuario` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COMMENT='tabla intermedio entre usuario y equipo';
 
--- ----------------------------
--- Records of t_usuario_equipo
--- ----------------------------
+-- Volcando datos para la tabla ati_proyecto.t_usuario_equipo: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `t_usuario_equipo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_usuario_equipo` ENABLE KEYS */;
 
--- ----------------------------
--- View structure for v_post_hashtag
--- ----------------------------
-DROP VIEW IF EXISTS `v_post_hashtag`;
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost`  VIEW `v_post_hashtag` AS SELECT
+
+-- Volcando estructura para vista ati_proyecto.v_post_hashtag
+/* Error de SQL (1356): View 'ati_proyecto.v_post_hashtag' references invalid table(s) or column(s) or function(s) or definer/invoker of view lack rights to use them */
+
+-- Volcando estructura para vista ati_proyecto.v_post_hashtag
+-- Eliminando tabla temporal y crear estructura final de VIEW
+DROP TABLE IF EXISTS `v_post_hashtag`;
+CREATE DEFINER=`root`@`localhost` VIEW `ati_proyecto`.`v_post_hashtag` AS SELECT
 t_post_categoria.id,
 t_post.id AS post_id,
 t_post.titulo,
@@ -192,3 +185,6 @@ INNER JOIN t_post_categoria ON t_post_categoria.post = t_post.id
 INNER JOIN t_categoria ON t_post_categoria.categoria = t_categoria.id
 ORDER BY
 t_post.fecha_creacion DESC ;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
